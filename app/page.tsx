cd691fd14b2c8d83f57a22f976c39feef88bbb82
cleriksson <erikssonclarkw@gmail.com>
2024-06-24 13:48:56 -0400

    some minor formatting changes to the basic site template

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  );
+
+    return (
+    
+        <main className={``}></main>
+    
+    );
+
 }
